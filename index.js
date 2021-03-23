a820af58f28f2ae972a7a3614accc66a49b67401
Adam Tatar <adam@wikileaf.com>
2021-03-23 18:21:44 -0400

    Function can now translate multiple words properly

--- index.js
+++ index.js
@@ -3,28 +3,28 @@ const prompts = require('prompts');
 (async () => {
     const response = await prompts({
         type: 'text',
-        name: 'meaning',
+        name: 'answer',
         message: 'Please enter a number followed by a string of letters. You may do this multiple times seperated by a space.'
     });
 
-    console.log(response.meaning);
-    arrayOfResponse = response.meaning.split(" ");
+    console.log(response.answer);
+    arrayOfResponse = response.answer.split(" ");
     arrayOfResponse.forEach(
         element => printAnswer(element));
 })();
 
 function printAnswer(param) {
     var characters = param.split("");
+    var number = characters[0];
+    //remove the number
+    characters.shift();
     console.log(characters);
     characters.forEach(
-        character => nextChar(character)    
+        character => nextChar(character,number)  
     )
-    // console.log(characters[1]);
-
-    // nextChar(characters[1], characters[0]);
-    // nextChar(characters[2], characters[0]);
 }
-//from Stack Overflow :)
-function nextChar(c, num=1) {
-    console.log(String.fromCharCode(c.charCodeAt(0) + 1));
+
+function nextChar(c="", num=1) {
+    var newLetter = String.fromCharCode(c.charCodeAt(0) + parseInt(num));
+    console.log(newLetter);
 }
